01f74092182e2d5234ad7bc308daa9e6f2bade3f
NKDfoolish <nguyenkhanhduy.dilinh@gmail.com>
2024-12-18 21:01:40 +0700

    check git operation

--- src/main/java/com/coffeeshop/mycoffee/configuration/SecurityConfig.java
+++ src/main/java/com/coffeeshop/mycoffee/configuration/SecurityConfig.java
@@ -93,6 +93,7 @@ public class SecurityConfig {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 
         corsConfiguration.addAllowedOrigin(SOURCE_FE);
+
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
 
